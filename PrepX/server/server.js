d43c2db9b9c1306cf354d8779ea8cab8e8e38ec9
Pranesh-I <ipranesh046@gmail.com>
2026-06-09 20:28:32 +0530

    Setup MongoDB Atlas connection

--- PrepX/server/server.js
+++ PrepX/server/server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();  // Load environment variables from .env file
 
 const app = require("./app");    // Import the Express app from app.js
+const connectDB = require("./config/db");  // Import the database connection function
+
+connectDB();  // Connect to the database
 
 const PORT = process.env.PORT || 5000;  // Use the PORT environment variable or default to 5000
 
