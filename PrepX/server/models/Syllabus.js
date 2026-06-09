d8bc7847b100d6451d1152c3c25b28a8b0916951
Pranesh-I <ipranesh046@gmail.com>
2026-06-09 20:51:24 +0530

    Create syllabus model

---
+++ PrepX/server/models/Syllabus.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const syllabusSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+
+    rawText: {
+      type: String,
+      default: "",
+    },
+
+    topics: [
+      {
+        type: String,
+      },
+    ],
+
+    subject: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Syllabus", syllabusSchema);
